9e797c5b148642d0e5d178f6da307c3650ac875f
thecakeisalie81 <www.lui81@gmail.com>
2026-07-07 20:29:21 -0600

    primer commit proyecto final curso apis

---
+++ ProyectoBazar/src/main/java/com/todocode/ProyectoBazar/ProyectoBazarApplication.java
@@ -0,0 +1,13 @@
+package com.todocode.ProyectoBazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoBazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoBazarApplication.class, args);
+	}
+
+}
---
+++ ProyectoBazar/src/test/java/com/todocode/ProyectoBazar/ProyectoBazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todocode.ProyectoBazar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
